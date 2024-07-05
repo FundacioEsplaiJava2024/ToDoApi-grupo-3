366ad8e715bcea0759fdedf164f5d79398c9b406
AnniaKuzmina <107867033+anniakuz@users.noreply.github.com>
2024-07-05 12:36:53 +0200

    Project structure with connection to DB

---
+++ src/main/java/todolist_grupo3/todolist_grupo3/entities/Task.java
@@ -0,0 +1,28 @@
+package todolist_grupo3.todolist_grupo3.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+
+}
