afeb25f4dcb742a5c5d2a0005cbdee8fa40407c0
AnniaKuzmina <107867033+anniakuz@users.noreply.github.com>
2024-07-05 09:37:38 +0200

    1st commit with a project structure

---
+++ src/main/java/todolist_grupo3/todolist_grupo3/TodolistGrupo3Application.java
@@ -0,0 +1,13 @@
+package todolist_grupo3.todolist_grupo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistGrupo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistGrupo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/todolist_grupo3/todolist_grupo3/TodolistGrupo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package todolist_grupo3.todolist_grupo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistGrupo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
