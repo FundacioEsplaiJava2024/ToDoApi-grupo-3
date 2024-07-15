c7598caef89893d973d0d97aecd468098a097e21
IzarReina <izarreina@gmail.com>
2024-07-15 11:20:48 +0200

    merge to deleted method to resolve issues

--- src/main/java/todolist_grupo3/controllers/TaskController.java
+++ src/main/java/todolist_grupo3/controllers/TaskController.java
@@ -4,26 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
-
 import todolist_grupo3.entities.Task;
 import todolist_grupo3.exception.HttpException;
 import todolist_grupo3.requests.CreateTaskRequest;
 import todolist_grupo3.requests.EditNameRequest;
-
 import todolist_grupo3.services.TaskService;
 
 
