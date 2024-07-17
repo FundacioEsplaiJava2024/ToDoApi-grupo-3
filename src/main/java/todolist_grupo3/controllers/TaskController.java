b52554969968d9a7bcb5f1e5a6e098959976efd9
David Freixes <dfis1996@gmail.com>
2024-07-17 10:44:32 +0200

    New changeState method with exceptions

--- src/main/java/todolist_grupo3/controllers/TaskController.java
+++ src/main/java/todolist_grupo3/controllers/TaskController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
+import todolist_grupo3.entities.State;
 import todolist_grupo3.entities.Task;
 import todolist_grupo3.exception.HttpException;
 import todolist_grupo3.requests.CreateTaskRequest;
@@ -36,7 +38,7 @@ public class TaskController {
             if (name == null || name.trim().isEmpty() || name.length() > 20) {
                 throw new HttpException(HttpStatus.BAD_REQUEST, "Error: name must be between 1 and 20 characters");
             }
-            else return ResponseEntity.status(HttpStatus.CREATED).body(taskService.createTask(createTaskRequest.getName()));
+            return ResponseEntity.status(HttpStatus.CREATED).body(taskService.createTask(createTaskRequest.getName()));
         } catch (HttpException e) {
             return ResponseEntity.status(e.getStatus()).body(e.getMessage());
         }
@@ -56,7 +58,7 @@ public class TaskController {
             if (task == null) {
                 throw new HttpException(HttpStatus.NOT_FOUND,"Error: Task not found");
             }
-            else return ResponseEntity.status(HttpStatus.OK).body(task);
+            return ResponseEntity.status(HttpStatus.OK).body(task);
         } catch (HttpException e) {
             return ResponseEntity.status(e.getStatus()).body(e.getMessage());
         }
@@ -70,10 +72,9 @@ public class TaskController {
             if (task == null) {
                 throw new HttpException(HttpStatus.NOT_FOUND,"Error: Task not found");
             }
-            else {
-                taskService.deleteTask(id);
-                return ResponseEntity.status(HttpStatus.OK).body("Task deleted");
-            }
+            
+            taskService.deleteTask(id);
+            return ResponseEntity.status(HttpStatus.OK).body("Task deleted");
         } catch (HttpException e) {
             return ResponseEntity.status(e.getStatus()).body(e.getMessage());
         }
@@ -91,9 +92,29 @@ public class TaskController {
             else if (name == null || name.trim().isEmpty() || name.length() > 20) {
                 throw new HttpException(HttpStatus.BAD_REQUEST, "Error: name must be between 1 and 20 characters");
             }
-            else return ResponseEntity.status(HttpStatus.OK).body(taskService.editTask(id, editNameRequest.getName()));
+            return ResponseEntity.status(HttpStatus.OK).body(taskService.editTask(id, editNameRequest.getName()));
         } catch (HttpException e) {
             return ResponseEntity.status(e.getStatus()).body(e.getMessage());
+        }
+    }
+
+    @PatchMapping("/task/{id}")
+    @CrossOrigin("*")
+    public ResponseEntity<?> changeTaskStateById(@PathVariable Integer id) {
+        try{
+            Task task = taskService.getTaskById(id);
+            if (task == null) {
+                throw new HttpException(HttpStatus.NOT_FOUND,"Error: Task not found");
+            }
+
+            State newState = taskService.changeStateTask(task);
+            if (newState.equals(task.getState())) {
+                throw new HttpException(HttpStatus.BAD_REQUEST, "Error: Task state not changed");
+            }
+            taskService.changeState(id, newState);
+            return ResponseEntity.status(HttpStatus.OK).body(taskService.getTaskById(id));
+        }catch(HttpException ex){
+            return ResponseEntity.status(ex.getStatus()).body(ex.getMessage());
         }
     }
 }   
--- src/main/java/todolist_grupo3/services/TaskServiceImpl.java
+++ src/main/java/todolist_grupo3/services/TaskServiceImpl.java
@@ -3,11 +3,13 @@ package todolist_grupo3.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
 import todolist_grupo3.entities.State;
 import todolist_grupo3.entities.Task;
+import todolist_grupo3.exception.HttpException;
 import todolist_grupo3.repo.TaskRepository;
 
 @Service
@@ -44,6 +46,29 @@ public class TaskServiceImpl implements TaskService{
         Task existingTask = taskRepository.findById(id).get();
         existingTask.setName(name);
         return taskRepository.save(existingTask);
+    }
+
+    @Override
+    public void changeState(Integer id, State state) {
+        Task task = getTaskById(id);
+        task.setState(state);
+        taskRepository.save(task); 
+    }
+
+    /*
+     * method for changeState(). In case when the state of the task is 'INCOMPLETE',
+     * returns State.COMPLETE. In other case means that the state is 'COMPLETE',
+     * so it returns State.INCOMPLETE
+     * it's private because it's for internal usage and we don't want to have access from another classes,
+     *  this way it also doesn't appear in TaskService. 
+     */
+    @Override
+    public State changeStateTask(Task task){
+        if(task.getState().equals(State.INCOMPLETE)){
+            return State.COMPLETE;
+        }else{
+            return State.INCOMPLETE;
+        }
     }
 
 }
