6647e36b556cf415605928a115b4dd665dba67e6
AnniaKuzmina <107867033+anniakuz@users.noreply.github.com>
2024-07-24 18:12:19 +0200

    changed RegisterRequest and created LoginRequest

---
+++ src/main/java/todolist_grupo3/requests/LoginRequest.java
@@ -0,0 +1,18 @@
+package todolist_grupo3.requests;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class LoginRequest {
+    @NotEmpty(message = "Name is mandatory")
+    @NotNull(message = "Name is mandatory")
+    @Size(min = 8, message = "Username should be 8 characters long minimum")
+    private String username;
+
+
+    @NotEmpty(message = "Password is mandatory")
+    @NotNull(message = "Password is mandatory")
+    @Size(min = 8, message = "Password should be 8 characters long minimum")
+    private String password;
+}
---
+++ src/main/java/todolist_grupo3/requests/RegisterRequest.java
@@ -0,0 +1,27 @@
+package todolist_grupo3.requests;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    @NotEmpty(message = "Name is mandatory")
+    @NotNull(message = "Name is mandatory")
+    @Size(min = 8, message = "Username should be 8 characters long minimum")
+    private String username;
+
+
+    @NotEmpty(message = "Password is mandatory")
+    @NotNull(message = "Password is mandatory")
+    @Size(min = 8, message = "Password should be 8 characters long minimum")
+    private String password;
+
+    @Email(message = "Email is not well formatted")
+    @NotEmpty(message = "Email is mandatory")
+    @NotNull(message = "Email is mandatory")
+    private String email;
+}
