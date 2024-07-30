a92ab334df8cbf12cb8f101841d7e02d338565d4
David Freixes <dfis1996@gmail.com>
2024-07-30 12:34:29 +0200

    New exception control in users and new conditions at the time of registration

--- src/main/java/todolist_grupo3/requests/RegisterRequest.java
+++ src/main/java/todolist_grupo3/requests/RegisterRequest.java
@@ -10,19 +10,19 @@ import lombok.Data;
 
 @Data
 public class RegisterRequest {
-    //@NotEmpty(message = "Name is mandatory")
-    //@NotNull(message = "Name is mandatory")
-    //@Size(min = 8, message = "Username should be 8 characters long minimum")
+    @NotEmpty(message = "Name is mandatory")
+    @NotNull(message = "Name is mandatory")
+    @Size(min = 8, message = "Username should be 8 characters long minimum")
     private String username;
 
 
-    //@NotEmpty(message = "Password is mandatory")
-    //@NotNull(message = "Password is mandatory")
-    //@Size(min = 8, message = "Password should be 8 characters long minimum")
+    @NotEmpty(message = "Password is mandatory")
+    @NotNull(message = "Password is mandatory")
+    @Size(min = 8, message = "Password should be 8 characters long minimum")
     private String password;
 
-    //@Email(message = "Email is not well formatted")
-    //@NotEmpty(message = "Email is mandatory")
-    //@NotNull(message = "Email is mandatory")
+    @Email(message = "Email is not well formatted")
+    @NotEmpty(message = "Email is mandatory")
+    @NotNull(message = "Email is mandatory")
     private String email;
 }
